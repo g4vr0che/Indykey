b949fe04ce24073c4d08d6bfb88b13d3b17bc4f5
Gaven Royer <gavroyer@gmail.com>
2019-09-05 11:28:08 -0600

    Make Python Package

---
+++ setup.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+
+"""
+ Indykey
+ A library for controlling keyboard backlight brightness
+
+ Copyright 2019 Gaven Royer <gavroyer@gmail.com>
+
+Permission to use, copy, modify, and/or distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright notice
+and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+THIS SOFTWARE.
+"""
+
+from setuptools import setup
+from distutils.cmd import Command
+import os
+import subprocess
+import sys
+
+FILETREE = os.path.dirname(os.path.abspath(__file__))
+TESTDIRS = ['indykey']
+
+def run_under_same_interpreter(opname, script, args):
+    """Re-run with the same as current interpreter."""
+    print('\n** running: {}...'.format(script), file=sys.stderr)
+    if not os.access(script, os.R_OK | os.X_OK):
+        print(
+            'ERROR: cannot read and execute: {!r}'.format(script),
+            file=sys.stderr
+        )
+        print(
+            'Consider running `setup.py test --skip-{}`'.format(opname),
+            file=sys.stderr
+        )
+        sys.exit(3)
+    cmd = [sys.executable, script] + args
+    print('check_call:', cmd, file=sys.stderr)
+    try:
+        subprocess.check_call(cmd)
+        print('** PASSED: {}\n'.format(script), file=sys.stderr)
+        return True
+    except subprocess.CalledProcessError:
+        print('** FAILED: {}\n'.format(script), file=sys.stderr)
+        return False
+
+def run_pytest():
+    """Do unit testing with pytest-3."""
+    return run_under_same_interpreter('test', '/usr/bin/pytest-3', [])
+
+def run_pyflakes3():
+    """Run a round of pyflakes3."""
+    script = '/usr/bin/pyflakes3'
+    names = [
+        'setup.py',
+    ] + DIRS
+    args = [os.path.join(TREE, name) for name in names]
+    return run_under_same_interpreter('flakes', script, args)
+
+class Test(Command):
+    """Lint checker and unit test runner."""
+    description = 'run pyflakes3'
+
+    user_options = [
+        ('skip-flakes', None, 'do not run pyflakes static checks'),
+        ('skip-test', None, 'Do run run unit tests')
+    ]
+
+    def initialize_options(self):
+        self.skip_sphinx = 0
+        self.skip_flakes = 0
+        self.skip_test = 0
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        if not self.skip_flakes:
+            pf3 = run_pyflakes3()
+        if not self.skip_test:
+            pt3 = run_pytest()
+        if not pt3 or not pf3:
+            print(
+                'ERROR: One or more tests failed with errors.'
+            )
+            exit(3)
+
+setup(
+    name='Indykey',
+    version='0.0.0',
+    author='Gaven Royer',
+    author_email='gavroyer@gmail.com',
+    url='https://github.com/g4vr0che/indykey',
+    description='A tool to set keyboard backlight colors and brightness',
+    download_url='https://github.com/g4vr0che/Indykey/releases',
+    test_require=['pytest', 'pyflakes'],
+    license='BSD-2',
+    packages=['indykey'],
+    data_files=[
+        ('/usr/lib/indykey/', ['data/indykey-service.py']),
+        ('/usr/share/dbus-1/system-services', ['data/com.github.g4vr0che.indykey.service']),
+        ('/usr/share/polkit-1/actions', ['data/com.github.g4vr0che.indykey.policy']),
+        ('/etc/dbus-1/system.d', ['data/com.github.g4vr0che.indykey.conf'])
+    ],
+    cmdclass={
+        'test': Test,
+    }
+)
